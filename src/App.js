3bfe5c3e4f036d4d5061aef2a412a5e8da6d5ab5
pavan <yennampavan01@gmail.com>
2023-04-15 00:43:07 +0530

    navbar deleted

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from './components/About';
 function App() {
   return (
     <>
-    <Navbar title="Textutiles" about="About us"/>
+    {/* <Navbar title="Textutiles" about="About us"/> */}
     <div className="container my-3">
       {/* <TextForm heading="Enter text to analyze"/> */}
     <About/>
